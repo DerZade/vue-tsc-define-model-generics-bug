aabee41b2bda5fbdb55b08db7670983a181834ef
DerZade <DerZade@gmail.com>
2025-01-10 07:45:16 +0100

    add test components

---
+++ src/components/TestWithDefaultModel.vue
@@ -0,0 +1,7 @@
+<template>
+    <div></div>
+</template>
+
+<script setup lang="ts" generic="T">
+const model = defineModel<T>();
+</script>
---
+++ src/components/TestWithoutModel.vue
@@ -0,0 +1,7 @@
+<template>
+    <div></div>
+</template>
+
+<script setup lang="ts" generic="T">
+defineProps<{ foo: T }>();
+</script>
